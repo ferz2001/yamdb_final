c8618a2ef12c7026de92955eca019a3bc2a21a03
ferz2001 <fmannanov01@gmail.com>
2022-01-11 13:03:32 +0300

    last

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -14,7 +14,7 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-	'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
